5fd2a3e0b705e3f99d15af34990a48d875d99a4d
Adonisvce <tbrownke@outlook.com>
2025-05-24 00:46:09 -0500

    Update notion_client.py

--- database/notion_client.py
+++ database/notion_client.py
@@ -1,1 +1,9 @@
-# Placeholder for notion_client.py in database
+
+# database/notion_client.py
+
+class NotionClient:
+    def __init__(self, token):
+        self.token = token
+
+    def get_data(self):
+        return "Dummy data from Notion"
