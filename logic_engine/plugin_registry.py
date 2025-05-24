be640062bb996f64b729d1cdf8e72c85933202b1
Adonisvce <tbrownke@outlook.com>
2025-05-24 02:52:27 -0500

    Update plugin_registry.py

--- logic_engine/plugin_registry.py
+++ logic_engine/plugin_registry.py
@@ -1,12 +1,3 @@
-# logic_engine/plugin_registry.py
+from logic_engine.registry import PluginRegistry
 
-hook_registry = {
-    "before_logic": [],
-    "after_logic": [],
-    "startup": [],
-    "shutdown": [],
-}
-
-def run_hooks(hook_name, *args, **kwargs):
-    for hook in hook_registry.get(hook_name, []):
-        hook(*args, **kwargs)
+plugin_registry = PluginRegistry()
