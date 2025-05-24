67b9088a5b2f1d2f305e6e115f26d383133984e2
Adonisvce <tbrownke@outlook.com>
2025-05-24 10:35:50 -0500

    Update

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,13 @@
-from .endpoint_router import register_endpoint_routes
-from .command_center import register_command_center_routes
+from .command_center import command_center_bp
+from .endpoint_router import endpoint_router_bp
+from .dynamic import dynamic_bp
+from .validate_plugins import validate_bp
+from .oauth import oauth_bp  # Make sure this line exists
 
 def register_all_routes(app):
-    register_endpoint_routes(app)
-    register_command_center_routes(app)
+    app.register_blueprint(command_center_bp)
+    app.register_blueprint(endpoint_router_bp)
+    app.register_blueprint(dynamic_bp)
+    app.register_blueprint(validate_bp)
+    app.register_blueprint(oauth_bp)  # Make sure this line exists
+
