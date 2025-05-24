da7f592a8b58775f3ad1894297ea3b6730f45b18
Adonisvce <tbrownke@outlook.com>
2025-05-24 11:38:39 -0500

    Updates

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,16 @@
+from flask import Flask
+
+# Import route blueprints
 from .command_center import command_center_bp
 from .endpoint_router import endpoint_router_bp
 from .dynamic import dynamic_bp
 from .validate_plugins import validate_bp
+from .logic_sync import logic_sync_bp
 
-def register_all_routes(app):
+def register_all_routes(app: Flask):
     app.register_blueprint(command_center_bp)
     app.register_blueprint(endpoint_router_bp)
     app.register_blueprint(dynamic_bp)
     app.register_blueprint(validate_bp)
+    app.register_blueprint(logic_sync_bp)
+
